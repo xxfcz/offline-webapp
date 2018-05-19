d02f253865ad5b79663319cab822d8780897ab66
xxf_cz <xxf_cz@163.com>
2018-05-19 17:17:16 +0800

    retrieving todo items

--- application.js
+++ application.js
@@ -8,7 +8,12 @@
       console.log("The database has been opened");
       input = document.querySelector('input');
       document.body.addEventListener('submit', onSubmit);
-    });
+    })
+    .then(databaseTotosGet)
+    .then(function(todos){
+      console.log(todos);
+    })
+    ;
 
   function onSubmit(e) {
     e.preventDefault();
@@ -50,4 +55,29 @@
     });
   }
 
+  function databaseTotosGet() {
+    return new Promise(function (resolve, reject){
+      var transaction = db.transaction(['todo'], 'readonly');
+      var store = transaction.objectStore('todo');
+      // Get everything in the store
+      var keyRange = IDBKeyRange.lowerBound(0);
+      var cursorRequest = store.openCursor(keyRange);
+      // This fires once per row in the store, so for simplicity collect the data
+      // in an array (data) and send it pass it in the resolve call in one go
+      var data = [];
+      cursorRequest.onsuccess = function(e){
+        var result = e.target.result;
+        // If there's data, add it to array
+        if(result){
+          data.push(result.value);
+          result.continue();
+        }else{
+          // Reach the end of the data
+          resolve(data);
+        }
+      };
+
+      cursorRequest.onerror = reject;
+    });
+  }
 }());
